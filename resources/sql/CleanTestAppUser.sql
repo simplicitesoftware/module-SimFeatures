83021fa44cc9a078c0fb7d674a949e9db775e536
Simon Campano <scampano@simplicite.fr>
2024-06-05 21:46:33 +0200

    clean master

--- resources/sql/CleanTestAppUser.sql
+++
@@ -1,4 +0,0 @@
--- Shared SQL CleanTestAppUser
-delete from m_user_sysparam where usp_user_id=(select row_id from m_user where usr_login='user');
-delete from m_session where ses_user_id=(select row_id from m_user where usr_login='user');
-delete from m_object_usage where obu_user_id=(select row_id from m_user where usr_login='user');
