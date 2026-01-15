da21b4616b9f2fbc5658ee125efbd10904308f58
Alistair Wheeler <awheeler@simplicite.fr>
2026-01-15 13:52:29 +0400

    :test_tube: user guide

--- others/playwright/tests/ft_63_features.spec.ts
+++ others/playwright/tests/ft_63_features.spec.ts
@@ -10,9 +10,9 @@ test.afterEach(async ({ page }) => {
   await logout(page);
 });
 
-test('Cartographical search', async ({ page }) => {
+/*test('Cartographical search', async ({ page }) => {
     //TODO: Implement cartographical search test
-});
+});*/
 
 test('Context menu on lists', async ({ page }) => {
     await openList(page, "FtDomain", "FtListItem");
@@ -62,14 +62,52 @@ test('Quick list ordering', async ({ page }) => {
     await expect(row2.locator("[data-field='ftLoOrder']")).toHaveText("2");
 
     // Reorder elements
-    await page.locator(".reorder").click();
     await page.locator('[data-action="reorder"]').last().hover();
     await page.mouse.down();
-    await page.mouse.move(330,319);
+    await page.mouse.move(330,300);
     await page.locator('.dock.before').first().hover();
     await page.mouse.up();
 
     // Expect row 2 to be before row 1
-    await expect(row2.locator("[data-field='ftLoOrder']")).toHaveText("2");
-    await expect(row1.locator("[data-field='ftLoOrder']")).toHaveText("3");
+    await expect(row2.locator("[data-field='ftLoOrder']")).toHaveText("1");
+    await expect(row1.locator("[data-field='ftLoOrder']")).toHaveText("2");
+});
+
+test('UI guides', async ({ page }) => {
+  await openList(page, "Ft63Features", "FtGuidedObject", "FtDomain.Ft63Features");
+  await page.locator(".list-actionbar [data-action='playguide']").click();
+
+  await expect(page.locator(".guide-overlay")).toBeVisible();
+
+  // Create step
+  let stepTooltip = page.locator(".guide-tooltip");
+  // Assert step tooltip is visible
+  await expect(stepTooltip.locator(".step-title")).toHaveText("Create item tooltip");
+  await expect(stepTooltip.locator(".card-body .step-desc")).toHaveText("Create item long tooltip");
+
+  await page.locator("[data-action='create']").click();
+
+  // Set user key step
+  stepTooltip = page.locator(".guide-tooltip");
+  await expect(stepTooltip.locator(".step-title")).toHaveText("Set user key tooltip");
+  await expect(stepTooltip.locator(".card-body .step-desc")).toHaveText("Set user key long tooltip");
+
+  await page.locator("#field_ftGoUserKey").fill("test");
+  await page.locator("#field_ftGoUserKey").blur();
+
+  // Save item step
+  stepTooltip = page.locator(".guide-tooltip");
+  await expect(stepTooltip.locator(".step-title")).toHaveText("Save item tooltip");
+  await expect(stepTooltip.locator(".card-body .step-desc")).toHaveText("Save item long tooltip");
+
+  await page.locator("[data-action='save']").click();
+
+  // End step
+  stepTooltip = page.locator(".guide-tooltip");
+  await expect(stepTooltip.locator(".step-title")).toHaveText("End of user guide");
+  await expect(stepTooltip.locator(".card-body .step-desc")).toHaveText("You have successfully been guided");
+  await page.locator(".guide-tooltip [data-action='nextstep']").click();
+
+  // Expect the guide overlay to be hidden and the toast message to be visible
+  await expect(page.locator(".guide-overlay")).not.toBeVisible();
 });
