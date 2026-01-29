ada1510cff562e7aeea715f15bdb84c16429bddc
Alistair Wheeler <awheeler@simplicite.fr>
2026-01-29 15:48:07 +0400

    :test_tube: list preferences

--- others/playwright/tests/lists.spec.ts
+++ others/playwright/tests/lists.spec.ts
@@ -10,6 +10,67 @@ test.afterEach(async ({ page }) => {
   await logout(page);
 });
 
+// List preferences
+test('FT_0025', async ({ page }) => {
+  await openList(page, "FtDomain", "FtListItem");
+
+  // Remove image field from list
+  await addOrRemoveImageFieldFromList(page, "right");
+  await expect(page.locator("th[data-name='ftLstImage']")).not.toBeVisible();
+
+  //Reset to original spec
+  await addOrRemoveImageFieldFromList(page, "left");
+  await expect(page.locator("th[data-name='ftLstImage']")).toBeVisible();
+
+  // Reorder column order
+  const headerCells = page.locator("thead tr.head th:not(.col-action)");
+  await expect(headerCells.nth(0)).toHaveAttribute("data-name", "ftLstCode");
+  await expect(headerCells.nth(1)).toHaveAttribute("data-name", "ftLstOrder");
+  await reorderColumns(page, "ftLstCode");
+  await expect(headerCells.nth(0)).toHaveAttribute("data-name", "ftLstOrder");
+  await expect(headerCells.nth(1)).toHaveAttribute("data-name", "ftLstCode");
+  await reorderColumns(page, "ftLstOrder"); //reset to original order
+
+  // Search prefs
+  await page.locator(".btn-search").click();
+  await expect(page.locator(".field-search[data-field='ftLstCode']")).toBeVisible();
+  await page.locator("[data-action='close']").click();
+  await toggleFieldInSearchPrefs(page, "right");
+  await page.locator(".btn-search").click();
+  await expect(page.locator(".field-search[data-field='ftLstCode']")).not.toBeVisible();
+  await page.locator("[data-action='close']").click();
+  await toggleFieldInSearchPrefs(page, "left");
+
+  async function toggleFieldInSearchPrefs(page: Page, direction: "left" | "right") {
+    await page.locator(".btn-plus").click();
+    await page.locator("[data-action='prefs']").click();
+    const dialog = page.locator("#dlgmodal_prefs");
+    await dialog.locator("[href='#preftab_1']").click();
+    await dialog.locator("option[value='ftLstCode']").click();
+    await dialog.locator(`#preftab_1 [data-action='${direction}']`).click();
+    await dialog.locator("[data-action='save']").click();
+  }
+  
+  async function reorderColumns(page: Page, field: String) {
+    await page.locator(".btn-plus").click();
+    await page.locator("[data-action='prefs']").click();
+    let dialog = page.locator("#dlgmodal_prefs");
+    await dialog.locator(`option[value='0:${field}']`).click();
+    await dialog.locator(`#preftab_0 [data-action='down']`).click();
+    await dialog.locator("[data-action='save']").click();
+  }
+
+  async function addOrRemoveImageFieldFromList(page: Page, direction: "left" | "right") {
+    await page.locator(".btn-plus").click();
+    await page.locator("[data-action='prefs']").click();
+    let dialog = page.locator("#dlgmodal_prefs");
+    await expect(dialog).toBeVisible();
+    await dialog.locator("option[value='0:ftLstImage']").click();
+    await dialog.locator(`#preftab_0 [data-action='${direction}']`).click();
+    await dialog.locator("[data-action='save']").click();
+  }
+});
+
 // Context menu on lists test
 test('CHG_00092', async ({ page }) => {
     await openList(page, "FtDomain", "FtListItem");
@@ -19,7 +80,7 @@ test('CHG_00092', async ({ page }) => {
     await skeletonDismissed(page);
     await page.locator("tr[data-rowid='0'] [data-field='ftLstType'] span.select2").click();
     await page.locator("tr[data-rowid='0'] [data-field='ftLstType'] .select2-results li").first().click();
-    await page.locator("tr[data-rowid='0'] [data-field='ftLstDescription'] iframe").click();
+    await page.locator("tr[data-rowid='0'] [data-field='ftLstDescription'] .htmleditor").click();
     await page.keyboard.type(code);
     await saveList(page);
     await page.locator('button[data-action="cancel"]').click();
@@ -91,7 +152,7 @@ test('Update on list', async ({ page }) => {
     await skeletonDismissed(page);
     await page.locator("tr[data-rowid='0'] [data-field='ftLstType'] span.select2").click();
     await page.locator("tr[data-rowid='0'] [data-field='ftLstType'] .select2-results li").first().click();
-    await page.locator("tr[data-rowid='0'] [data-field='ftLstDescription'] iframe").click();
+    await page.locator("tr[data-rowid='0'] [data-field='ftLstDescription'] .htmleditor").click();
     await page.keyboard.type(code);
     await saveList(page);
     await page.locator('button[data-action="cancel"]').click();
