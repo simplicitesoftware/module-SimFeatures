0026d6532ed5b4ff8c45f8abde5505fd93ecd12c
Alistair Wheeler <awheeler@simplicite.fr>
2026-02-16 11:13:45 +0400

    Datalink + Inlined object

---
+++ src/com/simplicite/objects/SimFeatures/FtObjInlineChild.java
@@ -0,0 +1,38 @@
+package com.simplicite.objects.SimFeatures;
+
+import com.simplicite.util.*;
+import com.simplicite.util.exceptions.SaveException;
+import com.simplicite.util.exceptions.ValidateException;
+
+/**
+ * Business object FtObjInlineChild
+ */
+public class FtObjInlineChild extends ObjectDB {
+	private static final long serialVersionUID = 1L;
+
+	private void updateParent(String desc) {
+	    String parentId = getFieldValue("ftObjInlineChildParentId");
+		try (BusinessObject parent = getGrant().getBusinessObject("FtObjInlineParent")) {
+			parent
+			.withAllAccess()
+			.forUpdate(parentId)
+			.withValue("ftObjInlineParentDescription", desc)
+			.validateAndSave();
+		}
+		catch (ValidateException | SaveException e) {
+			AppLog.error("Error updateParent", e);
+		}
+	}
+
+	@Override
+	public String postCreate() {
+		updateParent("Updated by child postCreate");
+	    return null;
+	}
+
+	@Override
+	public String postUpdate() {
+		updateParent("Updated by child postUpdate");
+	    return null;
+	}
+}
