ed7ec4d5dd280674237332fb68b1fe30a1019ef5
Alistair Wheeler <awheeler@simplicite.fr>
2026-02-17 17:28:33 +0400

    :test_tube: Select object test
    :test_tube: Select object test
    
    :test_tube: Select object test
    
    :test_tube: Select object test
    
    :test_tube: Select object test

---
+++ test/src/com/simplicite/tests/SimFeatures/FtSelectObjectTest.java
@@ -0,0 +1,30 @@
+package com.simplicite.tests.SimFeatures;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.simplicite.objects.SimFeatures.FtSelectObject;
+import com.simplicite.util.Grant;
+import com.simplicite.util.ObjectField;
+
+/**
+ * Unit tests FtSelectObjectTest
+ */
+public class FtSelectObjectTest {
+
+	private Grant getGrant() {
+		return Grant.getSystemAdmin();
+	} 
+
+	@Test
+	public void testPreSearch() {
+		FtSelectObject selectObjectDB = (FtSelectObject) getGrant().getTmpObject("FtSelectObject");
+		selectObjectDB.setFieldValue("ftSlcFieldDt", "2026-01-01");
+		selectObjectDB.preSearch();
+		ObjectField field = selectObjectDB.getField("ftSlcFieldDt");
+		assertNotNull("Filter date should not be null", field.getFilter());
+		assertNotNull("Filter date min should not be null", field.getFilterDateMin());
+		assertNotNull("Filter date max should not be null", field.getFilterDateMax());
+	}
+}
