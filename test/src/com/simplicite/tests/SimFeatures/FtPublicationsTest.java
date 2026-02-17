394e69a9db4c2950bd0c7e43c5b9fcf82e96dccf
Alistair Wheeler <awheeler@simplicite.fr>
2026-02-17 17:17:43 +0400

    :test_tube: Publication test

---
+++ test/src/com/simplicite/tests/SimFeatures/FtPublicationsTest.java
@@ -0,0 +1,26 @@
+package com.simplicite.tests.SimFeatures;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.simplicite.objects.SimFeatures.FtPublications;
+import com.simplicite.util.Grant;
+
+/**
+ * Unit tests FtPublicationsTest
+ */
+public class FtPublicationsTest {
+
+	private Grant getGrant() {
+		return Grant.getSystemAdmin();
+	} 
+
+	@Test
+	public void testPreSave() {
+		FtPublications publicationDB = (FtPublications) getGrant().getTmpObject("FtPublications");
+		publicationDB.setFieldValue("ftPubHtml", "<h1>Hello, World!</h1>");
+		publicationDB.preSave();
+		assertNotNull(publicationDB.getFieldValue("ftPubFile"));
+	}
+}
