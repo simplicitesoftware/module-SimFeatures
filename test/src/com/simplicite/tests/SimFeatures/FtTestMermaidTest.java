190516d6025ce2f2da716d9de7b6837c40eb27ff
Alistair Wheeler <awheeler@simplicite.fr>
2026-02-19 11:33:00 +0400

    :test_tube: Mermaid ext object unit test

--- test/src/com/simplicite/tests/SimFeatures/FtTestMermaidTest.java
+++ test/src/com/simplicite/tests/SimFeatures/FtTestMermaidTest.java
@@ -1,6 +1,7 @@
 package com.simplicite.tests.SimFeatures;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -21,7 +22,14 @@ public class FtTestMermaidTest {
 	public void testMermaidChartSpec() {
 		MermaidExternalObject mermaidExternalObject = (MermaidExternalObject) getGrant().getExternalObject("FtTestMermaid");
 		Parameters params = new Parameters();
+		mermaidExternalObject.init(params);
 		String mermaidChartSpec = mermaidExternalObject.getMermaidChartSpec(params);
 		assertNotNull("Mermaid chart spec should not be null", mermaidChartSpec);
+		
+		params.setParameter("object", "FtAttributes");
+		params.setParameter("inst", "tmp_FtAttributes");
+		mermaidChartSpec = mermaidExternalObject.getMermaidChartSpec(params);
+		assertTrue("Mermaid chart should contain object display", mermaidChartSpec.contains("Attributes"));
+		
 	}
 }
