d8aa2fb20a5ac116fa06ac6c4a02c25ef3316090
Alistair Wheeler <awheeler@simplicite.fr>
2026-09-24 17:10:04 +0200

    add redolog to purgeall sql

--- resources/sql/FtPurgeAll.sql
+++ resources/sql/FtPurgeAll.sql
@@ -25,3 +25,4 @@ truncate table ft_tgo_tag;
 delete from m_user where usr_login like 'pwuser_%';
 truncate table ft_history;
 truncate table ft_history_hist;
+delete from m_redolog where RLG_OBJECT like 'Ft%';
